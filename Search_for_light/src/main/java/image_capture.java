d6dd3bf3b24800a00c38e3b74200441b63e7a8eb
Eiad Azad <eiadmahamud2004@gmail.com>
2026-02-10 13:32:49 +0000

    Add class for photo capture Capture one image and shut down.

---
+++ Search_for_light/src/main/java/image_capture.java
@@ -0,0 +1,14 @@
+import swiftbot.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class image_capture  {
+	public static void main(String[] args) throws Exception {
+		SwiftBotAPI sb = swiftbot.SwiftBotAPI.INSTANCE;
+		BufferedImage img = sb.takeStill(ImageSize.SQUARE_720x720);
+		ImageIO.write(img, "jpg", new File("/data/home/pi/TestImage.jpg"));
+		
+		System.exit(1);
+	}
+}
