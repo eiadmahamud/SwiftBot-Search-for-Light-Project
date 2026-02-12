98de93820ae7e05532102e45d031b3c15596d500
Eiad Azad <eiadmahamud2004@gmail.com>
2026-02-12 12:14:35 +0000

    Add obstacle avoidance in the main code

--- Search_for_light/src/main/java/main_code/Blueprint.java
+++ Search_for_light/src/main/java/main_code/Blueprint.java
@@ -1,35 +1,41 @@
 package main_code;
 import java.util.*;
-
+import java.io.IOException;
+import obstacle_detection.Obstacle_avoidance;
 import obstacle_detection.Obstacle_detection;
 import swiftbot.*;
 
 public class Blueprint {
 	public static void main(String[] args) {
-		
+
 		SwiftBotAPI sb = swiftbot.SwiftBotAPI.INSTANCE;
 		long startTime = System.currentTimeMillis(); // Record the time when the program starts
 		int objectCount = 0; // Variable to store number of objects detected
 
 		final long five_Minutes = 5 * 60 * 1000; // Variable to store 5 minutes 
-		
+
 		while (objectCount < 5 && (System.currentTimeMillis() - startTime) < five_Minutes) {
-			
+			// From here, the obstacle detection and avoidance program starts
 			double distance = Obstacle_detection.ObsAvoid();
-			if (distance <= 50) {
+			while (distance <= 50) {
 				objectCount++;
-				// Code to move either left or right
+				try {
+					Obstacle_avoidance.avoidObstacle();
+				} catch (IOException e) {
+					System.out.println("Error during obstacle avoidance" + e.getMessage());
+				}
+				distance = Obstacle_detection.ObsAvoid();
 			}
-			
-			
-			
-		}
+			// The obstacle detection and avoidance program ends
+
+
+		} // main while loop ends here
 		System.out.println("5 objects detected within 5 minutes");
 		System.out.println("Enter TERMINATE to end program");
-		
+
 		Scanner sc = new Scanner(System.in);
 		String input = sc.nextLine();
-		
+
 		while (!input.equals("TERMINATE")) {
 			System.out.println("Invalid command. Please type TERMINATE");
 			input = sc.nextLine();
