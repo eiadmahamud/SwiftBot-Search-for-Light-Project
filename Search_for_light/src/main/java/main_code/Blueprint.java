3a60566c6242b94b56c9c33c01ea0d43139ad4e8
Eiad Azad <eiadmahamud2004@gmail.com>
2026-02-13 23:07:37 +0000

    Add obstacle handling in main code

--- Search_for_light/src/main/java/main_code/Blueprint.java
+++ Search_for_light/src/main/java/main_code/Blueprint.java
@@ -1,10 +1,9 @@
 package main_code;
 import java.util.*;
+import initialization.decision.movement.MovementFunctions;
 import initialization.decision.movement.Pixel_brightness;
 import java.io.IOException;
-import obstacle_detection.Obstacle_avoidance;
-import obstacle_detection.Obstacle_detection;
-import seek_light.Seek_light;
+import obstacle_detection.ObstacleHandling;
 import swiftbot.*;
 
 public class Blueprint {
@@ -14,35 +13,39 @@ public class Blueprint {
 		long startTime = System.currentTimeMillis(); // Record the time when the program starts
 		int objectCount = 0; // Variable to store number of objects detected
 		final long five_Minutes = 5 * 60 * 1000; // Variable to store 5 minutes 
-		
+
 		System.out.println("Welcome to SwiftBot Light-Seeking Program");
 		System.out.println("Press Button A To Start");
-		
+
 		// Enabling button
 		final boolean[] started = {false};
 		sb.enableButton(Button.A, () -> {
-		    System.out.println("Button A Pressed");
-		    started[0] = true;
+			System.out.println("Button A Pressed");
+			started[0] = true;
 		});
 		System.out.println("Waiting for user to press Button A...");
 		while (!started[0]) {
-		    try {
+			try {
 				Thread.sleep(50);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			} // Small delay to avoid busy-waiting
 		}
 
-		
+
 		System.out.println("Program starting...");
 		System.out.println("Initiating Program.");
-		
+
 		// Storing initials intensities
 		Pixel_brightness.pixelAnalysis();
-		
+
 		double initialLeftIntensity = Pixel_brightness.AvgLeft;
 		double initialCentreIntensity = Pixel_brightness.AvgCentre;
 		double initialRightIntensity = Pixel_brightness.AvgRight;
+
+		double HIGHEST = Pixel_brightness.Highest;
+		double SECONDHIGHEST = Pixel_brightness.SecondHighest;
+		double LOWEST = Pixel_brightness.Lowest;
 
 		System.out.println("Initial brightness values...");
 		System.out.println("Left: " + initialLeftIntensity);
@@ -53,10 +56,10 @@ public class Blueprint {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+
 		// Main loop
 		while (objectCount < 5 && (System.currentTimeMillis() - startTime) < five_Minutes) {
-			
+
 			// Capture and display intensities
 			Pixel_brightness.pixelAnalysis();
 			double left = Pixel_brightness.AvgLeft;
@@ -66,8 +69,32 @@ public class Blueprint {
 			System.out.println("Left: " + left);
 			System.out.println("Centre: " + centre);
 			System.out.println("Right: " + right);
-			
-			
+
+			// Obstacle handling
+			double distance = sb.useUltrasound();
+			if (distance < 50) {
+				ObstacleHandling.HandleObstacle();
+				System.out.println("WARNING! Obstacle Ahead At: " + distance + " cm!");
+				objectCount++;
+
+				if (left > right) {
+					System.out.println("Turning Left");
+					MovementFunctions.left();
+				} else if (right > left) {
+					System.out.println("Turning Right");
+					MovementFunctions.right();
+				} else { 
+					if (Math.random() < 0.5) {
+						System.out.println("Turning Left (Random)");
+						MovementFunctions.left();
+					} else {
+						System.out.println("Turning Right (Random)");
+						MovementFunctions.right();
+					}
+				}
+				continue;
+			}
+
 
 		} // main while loop ends here
 		System.out.println("5 objects detected within 5 minutes");
